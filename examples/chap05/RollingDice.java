113f0e95779324b966be7bcb2b72e97b683a5ef2
Marissa Schmidt <marissaschmidt@boisestate.edu>
2018-03-08 09:56:59 -0700

    Fixing RollingDice example to work with modified Die

--- examples/chap05/RollingDice.java
+++ examples/chap05/RollingDice.java
@@ -31,15 +31,11 @@ public class RollingDice
 
       	sum = die1.getFaceValue() + die2.getFaceValue();
       	System.out.println ("Sum: " + sum);
-//
-//      	int val1 = die1.roll();
-//      	int val2 = die2.roll();
-//      	sum = val1 + val2;
-      	
-      	sum = die1.roll() + die2.roll();
-      	
-      	
+
+      	die1.roll(); die2.roll();
       	System.out.println ("Die One: " + die1 + ", Die Two: " + die2);
+      	
+      	sum = die1.getFaceValue() + die2.getFaceValue();
       	System.out.println ("New sum: " + sum);
    	}
 }

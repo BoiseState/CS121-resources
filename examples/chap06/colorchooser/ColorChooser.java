0a515fddb4481f45ba3a279646aa2177bfcf11f8
Marissa Schmidt <marissaschmidt@boisestate.edu>
2018-04-02 13:44:32 -0600

    Updated colorchooser example to use extends

--- examples/chap06/colorchooser/ColorChooser.java
+++ examples/chap06/colorchooser/ColorChooser.java
@@ -1,39 +1,14 @@
 package colorchooser;
 
-import java.awt.Dimension;
-
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 /**
- * A basic color chooser GUI. This class extends JPanel, which means it is a JPanel.
- * We are extending the functionality of a JPanel by adding our own methods to it.
+ * A basic color chooser GUI.
  * 
  * @author CS121 Instructors
  */
-@SuppressWarnings("serial")
-public class ColorChooser extends JPanel
+public class ColorChooser
 {
-	private ColorGrid colorGrid;
-	
-	/**
-	 * Creates a new ColorChooser GUI.
-	 */
-	public ColorChooser()
-	{
-		// Sub-panel to display chosen color.
-		JPanel displayPanel = new JPanel();
-		displayPanel.setPreferredSize(new Dimension(300,300));
-		
-		// Sub-panel with grid of colors to choose from. The color grid needs to know which
-		// panel (displayPanel) to display the color on.
-		colorGrid = new ColorGrid(displayPanel);
-		
-		// Add sub-panels to this ColorChooser panel.
-		this.add(colorGrid.getJPanel());
-		this.add(displayPanel);
-	}
-	
 	/**
 	 * Initialize the GUI and make it visible.
 	 * @param args unused
@@ -43,7 +18,7 @@ public class ColorChooser extends JPanel
 		JFrame frame = new JFrame("Mini-ColorChooser");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		frame.getContentPane().add(new ColorChooser());
+		frame.getContentPane().add(new ColorChooserPanel());
 		
 		frame.pack();
 		frame.setVisible(true);
